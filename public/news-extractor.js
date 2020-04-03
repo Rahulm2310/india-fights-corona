8c480003b4c31c90aa43131325e28cdbe795e9b2
Rahulm2310 <rahulmohata2310@gmail.com>
2020-04-03 18:34:29 +0530

    prepared for deploying

--- public/news-extractor.js
+++ public/news-extractor.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
           var bottom_of_window = $(window).scrollTop() + $(window).height();
 
           /* If the object is completely visible in the window, fade it it */
-          if (bottom_of_window > bottom_of_object - 320) {
+          if (bottom_of_window > bottom_of_object - 500) {
             $(this).animate({ opacity: "1" }, 800);
           }
         });
