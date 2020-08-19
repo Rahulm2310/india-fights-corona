1677021f10774cc141db29b9f262b1b2d2c1a12c
Rahulm2310 <rahulmohata2310@gmail.com>
2020-08-20 00:23:22 +0530

    added charts

--- public/data-extractor.js
+++ public/data-extractor.js
@@ -1,16 +1,20 @@
-$(document).ready(function () {
-  let confirmed = 0;
-  let active = 0;
-  let recovered = 0;
-  let deaths = 0;
-  let confirmedToday = 0;
-  let recoveredToday = 0;
-  let deathsToday = 0;
-  let mortalityRate = 0;
-  let updatedTime = "";
-  let stateWiseData = [];
-  let districtWiseData = [];
+let confirmed = 0;
+let active = 0;
+let recovered = 0;
+let deaths = 0;
+let confirmedToday = 0;
+let recoveredToday = 0;
+let deathsToday = 0;
+let mortalityRate = 0;
+let tested = 0;
+let testedToday = 0;
+let updatedTime = "";
+let stateWiseData = [];
+let districtWiseData = [];
+let dateWiseData = [];
+let indiaTimeSeriesArray = [];
 
+$(document).ready(function () {
   fetch("https://api.covid19india.org/data.json")
     .then((res) => res.json())
     .then((data) => {
@@ -23,6 +27,8 @@ $(document).ready(function () {
       deathsToday = data.statewise[0].deltadeaths;
       updatedTime = data.statewise[0].lastupdatedtime;
       mortalityRate = ((deaths / confirmed) * 100).toFixed(2);
+      tested = data.tested[data.tested.length - 1].totalsamplestested;
+      testedToday = data.tested[data.tested.length - 1].samplereportedtoday;
 
       let timeFromNow = moment(updatedTime, "DD-MM-YYYY HH:mm:ss").fromNow();
 
@@ -31,6 +37,8 @@ $(document).ready(function () {
       $(".card-body .recovered").text(recovered);
       $(".card-body .deaths").text(deaths);
       $(".card-body .mortality-rate").text(`${mortalityRate} %`);
+      $(".card-body .tested").text(tested);
+      $(".card-body .tested-today").text(`(+${testedToday})`);
       $(".card-body .confirmed-today").text(`(+${confirmedToday})`);
       $(".card-body .recovered-today").text(`(+${recoveredToday})`);
       $(".card-body .deaths-today").text(`(+${deathsToday})`);
@@ -61,6 +69,13 @@ $(document).ready(function () {
 
       $(".statewise-table").html(stateTableRows.join(""));
 
+      // ========================= CHARTS =====================================
+
+      indiaTimeSeriesArray = data.cases_time_series;
+      console.log(indiaTimeSeriesArray);
+      createChart(indiaTimeSeriesArray);
+
+      // ==================== DATE WISE TABLE ==========================
       dateTableRows = dateWiseData.map(
         (item) => `<tr class="table-secondary">
         <th scope="row">${item.date}</th>
@@ -76,6 +91,45 @@ $(document).ready(function () {
       $(".datewise-table").html(dateTableRows.join(""));
     });
 
+  $(".chart-control-btns .btn").on("click", function () {
+    const chartType = document.querySelector("select.dropdown-container").value;
+    console.log("chart type:", chartType);
+    console.log("pressed");
+    const time = $(this).text().trim();
+    console.log(time);
+    if (time == "Beginning") {
+      createChart(indiaTimeSeriesArray, chartType);
+      console.log(indiaTimeSeriesArray);
+    } else if (time == "3 months") {
+      console.log(
+        indiaTimeSeriesArray.filter(
+          (item, index) => index > indiaTimeSeriesArray.length - 91
+        )
+      );
+      createChart(
+        indiaTimeSeriesArray.filter(
+          (item, index) => index > indiaTimeSeriesArray.length - 91
+        ),
+        chartType
+      );
+    } else if (time == "1 month") {
+      console.log(
+        indiaTimeSeriesArray.filter(
+          (item, index) => index > indiaTimeSeriesArray.length - 31
+        )
+      );
+      createChart(
+        indiaTimeSeriesArray.filter(
+          (item, index) => index > indiaTimeSeriesArray.length - 31
+        ),
+        chartType
+      );
+    }
+  });
+
+  // ==============================================================
+
+  // ============================== DISTRICT WISE TABLE ========================
   fetch("https://api.covid19india.org/state_district_wise.json")
     .then((res) => res.json())
     .then((data) => {
@@ -159,3 +213,127 @@ $(document).ready(function () {
     $(".state-updated-time").text(`( Last updated ${updatedFromNow} )`);
   });
 });
+
+function selectChart(value) {
+  console.log(value);
+  // console.log(indiaTimeSeriesArray);
+  // if (value == "pie") {
+  //   $(".chart-control-btns input").attr("disabled", true);
+  //   createPieChart();
+  // }
+  if (value == "radar") {
+    $(".chart-control-btns input").attr("disabled", true);
+    createChart(
+      indiaTimeSeriesArray.filter(
+        (item, index) => index > indiaTimeSeriesArray.length - 31
+      ),
+      value
+    );
+  } else {
+    $(".chart-control-btns input").attr("disabled", false);
+    createChart(indiaTimeSeriesArray, value);
+  }
+}
+
+function createChart(indiaTimeSeries, chartType = "line") {
+  const totalConfirmed = indiaTimeSeries.map((item) => item.totalconfirmed);
+  const totalActive = indiaTimeSeries.map(
+    (item) => item.totalconfirmed - item.totalrecovered - item.totaldeceased
+  );
+  const totalRecovered = indiaTimeSeries.map((item) => item.totalrecovered);
+  const totalDeaths = indiaTimeSeries.map((item) => item.totaldeceased);
+  const dates = indiaTimeSeries.map((item, index) => {
+    // if (index % 30 == 0) {
+    return item.date;
+    // }
+  });
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: chartType == "area" ? "line" : chartType,
+
+    // The data for our dataset
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Confirmed",
+          backgroundColor:
+            chartType == "line" ? "transparent" : "rgba(213,102,98,0.7)",
+          borderColor: "#c52d28",
+          data: totalConfirmed,
+        },
+        {
+          label: "Active",
+          backgroundColor:
+            chartType == "line" ? "transparent" : "rgba(213,102,98,0.7)",
+          borderColor: "#DF691A",
+          data: totalActive,
+        },
+
+        {
+          label: "Recovered",
+          backgroundColor:
+            chartType == "line" ? "transparent" : "rgba(92,184,92,0.7)",
+          borderColor: "#5cb85c",
+          data: totalRecovered,
+        },
+        {
+          label: "Deaths",
+          backgroundColor: chartType == "line" ? "transparent" : "grey",
+          borderColor: "#4E5D6C",
+          data: totalDeaths,
+        },
+      ],
+    },
+
+    // Configuration options go here
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      responsiveAnimationDuration: 1000,
+      color: "#fff",
+    },
+  });
+}
+
+// const createPieChart = () => {
+//   var ctx = document.getElementById("myChart").getContext("2d");
+//   var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: "pie",
+
+//     // The data for our dataset
+//     data: {
+//       labels: ["Active", "Recovered", "Deaths"],
+//       datasets: [
+//         {
+//           label: "Active",
+//           backgroundColor: "rgba(213,102,98,0.7)",
+//           borderColor: "#c52d28",
+//           data: [{ active, backgroundColor: "red" }],
+//         },
+//         {
+//           label: "Recovered",
+//           backgroundColor: "green",
+//           borderColor: "green",
+//           data: [recovered],
+//         },
+//         {
+//           label: "Recovered",
+//           backgroundColor: "grey",
+//           borderColor: "grey",
+//           data: [deaths],
+//         },
+//       ],
+//     },
+
+//     // Configuration options go here
+//     options: {
+//       responsive: true,
+//       maintainAspectRatio: true,
+//       responsiveAnimationDuration: 1000,
+//       // color: "#fff",
+//     },
+//   });
+// };
